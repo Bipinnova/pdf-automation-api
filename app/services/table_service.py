27c13a6ee70b1cc3401883606187adf1815f638e
Bipin <bipinyadav1030@gmail.com>
2026-06-22 13:44:01 +0530

    added

--- app/services/table_service.py
+++ app/services/table_service.py
@@ -1,27 +1,52 @@
+import os
 import tabula
+import pandas as pd
 
 
 def extract_table(pdf_path):
 
+    # Create outputs folder if not exists
+    os.makedirs("outputs", exist_ok=True)
+
+    # Extract all tables from all pages
     tables = tabula.read_pdf(
         pdf_path,
-        pages="1"
+        pages="all",
+        multiple_tables=True
     )
 
-    csv_path = "outputs/output.csv"
-    excel_path = "outputs/output.xlsx"
+    if not tables:
+        return {
+            "message": "No tables found in PDF"
+        }
 
-    tables[0].to_csv(
-        csv_path,
-        index=False
-    )
+    excel_path = "outputs/all_tables.xlsx"
 
-    tables[0].to_excel(
-        excel_path,
-        index=False
-    )
+    csv_files = []
+
+    with pd.ExcelWriter(excel_path) as writer:
+
+        for index, table in enumerate(tables, start=1):
+
+            csv_path = f"outputs/table_{index}.csv"
+
+            # Save CSV
+            table.to_csv(
+                csv_path,
+                index=False
+            )
+
+            csv_files.append(csv_path)
+
+            # Save Excel Sheet
+            table.to_excel(
+                writer,
+                sheet_name=f"Table_{index}",
+                index=False
+            )
 
     return {
-        "csv": csv_path,
-        "excel": excel_path
+        "total_tables": len(tables),
+        "csv_files": csv_files,
+        "excel_file": excel_path
     }
